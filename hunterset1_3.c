b594997d15e2d74d0c2136721e0c643e8404fa0d
AgrawalPragati <35077511+AgrawalPragati@users.noreply.github.com>
2018-05-06 21:17:27 +0530

    Add files via upload

---
+++ hunterset1_3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int sort(int *arr,int n)
+{
+	int i,j,temp;
+	 for(i =0; i<n-1;i++)
+    {
+        for(j = 0;j<n-i-1;j++)
+        {
+            if( arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            } 
+        }
+}}
+void main()
+{
+   int n,i,k=0;
+   scanf("%d",&n);
+   int arr[10],temp[10];
+   for(i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+  	if(arr[i]==i)
+  	temp[k++]=arr[i];
+}
+i=0;
+  if(temp[i]==NULL)
+  {
+  	printf("-1");
+  }
+  sort(temp,k);
+  for(i=0;i<k;i++)
+  printf("%d ",temp[i]);
+}
+ 
